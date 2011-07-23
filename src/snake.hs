98ce9459693b4543c54243ab773ecdef14beef4b
Callum Lowcay <callum@callumscode.com>
2011-07-23 19:10:21 +1200

    create SDL boilerplate

--- src/snake.hs
+++ src/snake.hs
@@ -1,5 +1,30 @@
 module Main where
 
+import Graphics.UI.SDL
+import IO
+import Maybe
+import Time
+
 main :: IO ()
-main = putStrLn "Hello"
+main = do
+	initSDL
+
+initSDL :: IO ()
+initSDL = do
+	Graphics.UI.SDL.init [InitVideo]
+	setVideoMode 680 480 32 [HWSurface, DoubleBuf]
+	time <- getClockTime
+	mainLoop time
+	quit
+
+mainLoop :: ClockTime -> IO ()
+mainLoop time0 = do
+	time <- getClockTime
+	let
+		timeDiff = diffClockTimes time time0
+		delay = max ((tdPicosec timeDiff) + (toInteger$ tdSec timeDiff) * 10^12) 0
+	event <- pollEvent
+	case event of
+		Quit -> return ()
+		_ -> mainLoop time
 
