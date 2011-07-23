235ec3c0f9f5c78f4269ae925370f0b23ca7c9ae
Callum Lowcay <callum@callumscode.com>
2011-07-23 18:55:32 +1200

    clones: reboot clones project in haskell

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" = "--debug" ]; then debug="-DDEBUG"; fi
+
+if [ ! -d build/assets/gfx ]; then mkdir -p build/assets/gfx; fi
+cp src_assets/gfx/* build/assets/gfx
+
+if [ ! -d build ]; then mkdir build; fi
+for exe in snake; do
+	ghc -cpp -DASSET_PREFIX="\"./assets/\"" $debug -ibuild:src -outputdir build --make src/$exe.hs -o build/$exe
+done
+
