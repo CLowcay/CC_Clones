5e6c87620710082e9c52a8899cdf67020d0727cd
Callum Lowcay <callum@callumscode.com>
2011-08-16 19:58:08 +1200

    add liscence headers

--- src/Common/Events.hs
+++ src/Common/Events.hs
@@ -1,3 +1,21 @@
+{-
+CC_Clones - Classic games reimplemented
+© Callum Lowcay 2006-2011
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 module Common.Events where
 
 import Graphics.UI.SDL
