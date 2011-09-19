6aaf68302848365f60e16b8093214551ae5a3a69
Callum Lowcay <callum@callumscode.com>
2011-09-20 11:58:22 +1200

    refactoring based on hlint

--- src/Common/Counters.hs
+++ src/Common/Counters.hs
@@ -16,8 +16,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Common.Counters (
-	CounterState(..),
+	CounterState,
 	initCounter, updateCounter,
 	addCounter, subCounter, setCounter, resetCounter,
 	renderCounter
@@ -28,23 +31,23 @@ import Control.Monad
 import Graphics.UI.SDL
 
 data CounterState = CounterState {
-	cs_digits :: Animation,
-	cs_display :: Int,
-	cs_target :: Int,
-	cs_ttFrameSwap :: Integer,
-	cs_framesToAlignment :: Int,
-	cs_nDigits :: Int,
-	cs_changedDigits :: [Bool]
+	digits :: Animation,
+	display :: Int,
+	target :: Int,
+	ttFrameSwap :: Integer,
+	framesToAlignment :: Int,
+	nDigits :: Int,
+	changedDigits :: [Bool]
 } deriving (Show)
 
 -- Initialise a new CounterState
 initCounter :: Animation -> Int -> CounterState
 initCounter digits nDigits = CounterState {
-	cs_digits = digits,
-	cs_display = 0, cs_target = 0,
-	cs_ttFrameSwap = 0, cs_framesToAlignment = 0,
-	cs_nDigits = nDigits,
-	cs_changedDigits = take nDigits $ repeat False
+	digits = digits,
+	display = 0, target = 0,
+	ttFrameSwap = 0, framesToAlignment = 0,
+	nDigits = nDigits,
+	changedDigits = replicate nDigits False
 }
 
 -- The delay for counter frames, in picoseconds
@@ -53,84 +56,75 @@ frameDelay = ((1::Integer) * 10^12) `div` (4 * 18)
 
 -- Update a counter state based on a time delta
 updateCounter :: Integer -> CounterState -> CounterState
-updateCounter delay state =
+updateCounter delay (state@CounterState {..}) =
 	let
-		anidiff = (cs_ttFrameSwap state) - delay
+		anidiff = ttFrameSwap - delay
 		advanceFrames = fromInteger$ if anidiff < 0
-			then ((abs anidiff) `div` frameDelay) + 1 else 0
+			then abs anidiff `div` frameDelay + 1 else 0
 		offset' = framesToAlignment - advanceFrames
 		framesToAlignment' = if offset' < 0
 			then offset' `mod` 18 else offset'
 		advanceDigits = if offset' < 0
-			then ((abs offset') `div` 18) + 1 else 0
+			then abs offset' `div` 18 + 1 else 0
 		display' = if target > display
 			then min target (display + advanceDigits)
 			else if target < display
 				then max target (display - advanceDigits)
 				else display
 	in state {
-		cs_framesToAlignment =
+		framesToAlignment =
 			if target == display && framesToAlignment == 0
 				then 0 else framesToAlignment',
-		cs_ttFrameSwap = if anidiff < 0
+		ttFrameSwap = if anidiff < 0
 			then frameDelay + (anidiff `mod` frameDelay)
 			else anidiff,
-		cs_display = display',
-		cs_changedDigits = if framesToAlignment' > framesToAlignment
-			then map
-				(\(a, b) -> a /= b) $ zip
-					(fixedFieldLeft nDigits 0 $ toDec display)
-					(fixedFieldLeft nDigits 0 $ toDec display')
-			else cs_changedDigits state
+		display = display',
+		changedDigits = if framesToAlignment' > framesToAlignment
+			then zipWith (/=)
+				(fixedFieldLeft nDigits 0 $ toDec display)
+				(fixedFieldLeft nDigits 0 $ toDec display')
+			else changedDigits
 	}
-	where
-		framesToAlignment = cs_framesToAlignment state
-		nDigits = cs_nDigits state
-		target = cs_target state
-		display = cs_display state
 
 -- Add a number to the counter
 addCounter :: Int -> CounterState -> CounterState
-addCounter n (state@(CounterState {cs_target = target})) =
-	state {cs_target = target + n}
+addCounter n (state@(CounterState {target})) =
+	state {target = target + n}
 
 -- Subtract a number from the counter
 subCounter :: Int -> CounterState -> CounterState
-subCounter n (state@(CounterState {cs_target = target})) =
-	state {cs_target = target - n}
+subCounter n (state@(CounterState {target})) =
+	state {target = target - n}
 
 -- Set the value of the counter
 setCounter :: Int -> CounterState -> CounterState
-setCounter n state = state {cs_target = n}
+setCounter n state = state {target = n}
 
 -- Set the value of the counter, and do not animate the return
 resetCounter :: Int -> CounterState -> CounterState
 resetCounter n state = state {
-	cs_target = n, cs_display = n,
-	cs_ttFrameSwap = 0, cs_framesToAlignment = 0
+	target = n, display = n,
+	ttFrameSwap = 0, framesToAlignment = 0
 }
 
 -- Render the counter
 renderCounter :: Int -> Int -> CounterState -> IO ()
-renderCounter x y state = do
+renderCounter x y (state@CounterState {..}) = do
 	let
-		digits = fixedFieldLeft nDigits 0 $ toDec$ cs_display state
+		ddigits = fixedFieldLeft nDigits 0 $ toDec display
 		digitOffsets = zip
 			(reverse [0..(nDigits - 1)])
-			(map (digitOffset) (zip changedDigits digits))
+			(map digitOffset (zip changedDigits ddigits))
 
-	display <- getVideoSurface
+	ddisplay <- getVideoSurface
 
-	forM_ digitOffsets $ \(iDigit, offset) -> do
-		renderAnimationLoopV display 0
-			(x + (iDigit * 20)) y offset (cs_digits state)
+	forM_ digitOffsets $ \(iDigit, offset) ->
+		renderAnimationLoopV ddisplay 0
+			(x + (iDigit * 20)) y offset digits
 
 	return ()
 
 	where
-		framesToAlignment = cs_framesToAlignment state
-		nDigits = cs_nDigits state
-		changedDigits = cs_changedDigits state
 		digitOffset (changed, d) = 
 			((d * 18) - (if changed then framesToAlignment else 0)) `mod` 180
 	
@@ -142,5 +136,5 @@ fixedFieldLeft n padding xs = take n (xs ++ repeat padding)
 toDec :: Int -> [Int]
 toDec n
 	| n < 10 = [n]
-	| otherwise = (n `rem` 10):(toDec (n `div` 10))
+	| otherwise = (n `rem` 10) : toDec (n `div` 10)
 
